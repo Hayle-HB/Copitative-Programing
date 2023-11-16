b7bcca34559864738f47ea4235f52e5950d8022e
Haylemeskel Haylemariam <137502715+lij-haylew@users.noreply.github.com>
2023-11-16 11:54:21 +0300

    Add files via upload
    Write a Java program that uses a Vector to store a collection of words. The program should prompt the user to enter words until they enter the word "done" (case-insensitive). Afterward, display the words in reverse order, excluding the word "done." Ensure that the word "done" is not included in the final output.

--- Java_Code.java
+++ Java_Code.java
@@ -5,10 +5,10 @@ public class Java_Code {
 
   public static Vector<String> Reverse_Word(Vector<String> Words) {
     Vector<String> Reverse = new Vector<>();
-      
-      for(int i =Words.size() -1; i >=0; i-- ){
-        Reverse.add(Words.get(i));
-      }
+
+    for (int i = Words.size() - 1; i >= 0; i--) {
+      Reverse.add(Words.get(i));
+    }
 
     return Reverse;
   }
@@ -18,11 +18,11 @@ public class Java_Code {
     Vector<String> Words = new Vector<>();
     String terminateWord = "";
 
-    while(true){
+    while (true) {
 
       System.out.println("Enter Word or \"done\" to Stop: ");
       terminateWord = scan.nextLine();
-      if(terminateWord.toLowerCase().equals("done")){
+      if (terminateWord.toLowerCase().equals("done")) {
         break;
       }
 
@@ -30,9 +30,7 @@ public class Java_Code {
         Words.add(terminateWord);
       }
     }
-  
 
-    
     System.out.println("*********************\n" + Reverse_Word(Words));
 
     scan.close();
